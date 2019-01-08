c72d8bce0b8a218b2fc309ccdb76686774f2e3ad
ljlyixiang <1126200393@qq.com>
2019-01-08 19:09:09 +0800

    删除原有的

--- product/src/main/java/com/imooc/product/service/ProductCategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.imooc.product.service;
-
-import com.imooc.product.dataObject.ProductCategory;
-
-import java.util.List;
-
-public interface ProductCategoryService {
-		List<ProductCategory> findByCategoryTypeIn(List<Integer> productCategoryTypeList);
-}
